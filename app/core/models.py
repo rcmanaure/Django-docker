95a204c2392c2e5534354754596463fb49b4575f
rcmanaure <rcmanaure@gmail.com>
2023-04-13 16:44:51 -0400

    addef recipe app, tests and models

--- app/core/models.py
+++ app/core/models.py
@@ -4,14 +4,13 @@ Database models.
 # import uuid
 # import os
 
-# from django.conf import settings
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
-
+from django.db import models
 
 # def recipe_image_file_path(instance, filename):
 #     """Generate file path for new recipe image."""
@@ -57,23 +56,24 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
 
 
-# class Recipe(models.Model):
-#     """Recipe object."""
-#     user = models.ForeignKey(
-#         settings.AUTH_USER_MODEL,
-#         on_delete=models.CASCADE,
-#     )
-#     title = models.CharField(max_length=255)
-#     description = models.TextField(blank=True)
-#     time_minutes = models.IntegerField()
-#     price = models.DecimalField(max_digits=5, decimal_places=2)
-#     link = models.CharField(max_length=255, blank=True)
-#     tags = models.ManyToManyField('Tag')
-#     ingredients = models.ManyToManyField('Ingredient')
-#     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+class Recipe(models.Model):
+    """Recipe object."""
 
-#     def __str__(self):
-#         return self.title
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    # tags = models.ManyToManyField("Tag")
+    # ingredients = models.ManyToManyField("Ingredient")
+    # image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self):
+        return self.title
 
 
 # class Tag(models.Model):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,13 +2,11 @@
 Tests for models.
 """
 # from unittest.mock import patch
-# from decimal import Decimal
+from decimal import Decimal
 
-from django.test import TestCase
+from core import models
 from django.contrib.auth import get_user_model
-
-# from core import models
-
+from django.test import TestCase
 
 # def create_user(email="user@example.com", password="testpass123"):
 #     """Create a return a new user."""
@@ -57,21 +55,21 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
-    # def test_create_recipe(self):
-    #     """Test creating a recipe is successful."""
-    #     user = get_user_model().objects.create_user(
-    #         'test@example.com',
-    #         'testpass123',
-    #     )
-    #     recipe = models.Recipe.objects.create(
-    #         user=user,
-    #         title='Sample recipe name',
-    #         time_minutes=5,
-    #         price=Decimal('5.50'),
-    #         description='Sample receipe description.',
-    #     )
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "testpass123",
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Sample recipe name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample receipe description.",
+        )
 
-    #     self.assertEqual(str(recipe), recipe.title)
+        self.assertEqual(str(recipe), recipe.title)
 
     # def test_create_tag(self):
     #     """Test creating a tag is successful."""
